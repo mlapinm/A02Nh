c880e769a770d3920260d38fad7d0c108f2526c2
mlapinm <mlapin@rambler.ru>
2020-05-16 12:43:34 +0300

    111

---
+++ json/data.js
@@ -0,0 +1,8 @@
+dataHandler({
+  "name": "Michael",
+  "title": "Author",
+  "links": [
+  {"spinghost": "http://f0437348.xsph.ru/"},
+  {"youtube": "https://www.youtube.com/channel/UCxqme2Kg6j40T6gpxelc1YA"}
+  ]
+});
